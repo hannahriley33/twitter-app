3906ba062899a5bf7ae61e4be4661602de3389dd
Hannah Riley <riley.hannahm@gmail.com>
2020-03-25 08:24:19 -0700

    dev branch created

--- __tests__/tweet.test.js
+++ __tests__/tweet.test.js
@@ -4,61 +4,99 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/Tweet');
+
 describe('app routes', () => {
   beforeAll(() => {
     connect();
   });
+
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+
   afterAll(() => {
     return mongoose.connection.close();
   });
+
   it('creates a new tweet', () => {
-    return Tweet.create({
-      handle: '@testtweet',
-      text: 'test tweet'
-    })
-      .then(() => {
-        request(app)
-          .post('/api/v1/tweets')
-          .then(res => {
-            expect(res.body).toEqual({
-              _id: expect.any(String),
-              handle: '@testtweet',
-              text: 'test tweet',
-              __v: 0
-            });
-          });
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({
+        handle: '@testtweet',
+        text: 'test tweet'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: '@testtweet',
+          text: 'test tweet',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all tweets', async() => {
+    const tweets = await Tweet.create([
+      { handle: '@testing1', text: 'testing1' },
+      { handle: '@testing2', text: 'testing2' },
+      { handle: '@testing3', text: 'testing3' }
+    ]);
+    return request(app)
+      .get('/api/v1/tweets')
+      .then(res => {
+        tweets.forEach((tweet) => {
+          expect(res.body).toContainEqual(
+            { _id: tweet._id.toString(), handle: tweet.handle, text: tweet.text,  __v: 0 }   
+          );
+        });
+      });
+  });
+
+  it('gets tweet by id', async() => {
+    const tweet = await Tweet.create({ 
+      handle: '@testing1', text: 'testing1' 
+    });
+    return request(app)
+      .get(`/api/v1/tweets/${tweet._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: tweet._id.toString(),
+          handle: tweet.handle, 
+          text: tweet.text,  
+          __v: 0
+        });
+      });
+  });
+
+  it('updates tweet by id', async() => {
+    const tweet = await Tweet.create({ 
+      handle: '@testing1', text: 'testing1' 
+    });
+    return request(app)
+      .patch(`/api/v1/tweets/${tweet._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: tweet._id.toString(),
+          handle: tweet.handle, 
+          text: tweet.text,  
+          __v: 0
+        });
       });
   });
   
-  it('gets all tweets', () => {
-    return Tweet.create([
-      {
-        handle: '@testing',
-        text: 'testing'
-      },
-      {
-        handle: '@testing',
-        text: 'testing'
-      },
-      {
-        handle: '@testing',
-        text: 'testing'
-      }
-    ])
-    .then(() => {
-        request(app)
-        .get('/api/v1/tweets')
-        .then(res => {
-            expect(res.body).toContainEqual({
-              _id: expect.any(String),
-              handle: expect.any(String),
-              text: expect.any(String),
-              __v: 0
+  it('deletes tweet by id', async() => {
+    const tweet = await Tweet.create({ 
+      handle: '@testing1', text: 'testing1' 
+    });
+    return request(app)
+      .delete(`/api/v1/tweets/${tweet._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: tweet._id.toString(),
+          handle: tweet.handle, 
+          text: tweet.text,  
+          __v: 0
         });
       });
   });
 });
-
